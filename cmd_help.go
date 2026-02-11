7a5d06407e446b88cf4479dfd035af4c0410abc6
Denkkou <JoeSchoDev@gmail.com>
2026-02-11 17:28:51 +0200

    Restructured code into dedicated files to follow Lane Wagner's example. Implemented 'help' and 'exit' commands.

---
+++ cmd_help.go
@@ -0,0 +1,16 @@
+package main
+import (
+	"fmt"
+)
+
+func commandHelp() error {
+	fmt.Println("\nWelcome to the Pokedex!")
+
+	// Generate a usage section by iterating command registry
+	fmt.Println("Usage:\n")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	
+	return nil
+}
--- repl.go
+++ repl.go
@@ -1,11 +1,65 @@
 package main
 
 import (
+	"fmt"
+	"bufio"
 	"strings"
+	"os"
 )
+
+// Run the REPL with this function
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		
+		// Prepare user input
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+		
+		// Handle commands
+		if cmd, ok := getCommands()[input[0]]; ok {
+			// Capture callback func's error
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command\n")
+			continue
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	cleanedTextSlice := strings.Fields(strings.ToLower(text))
 	return cleanedTextSlice
 }
 
+// Each cli command follows this structure
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+func getCommands() map[string]cliCommand {
+	// Define each cli command in this map
+	return map[string]cliCommand {
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+}
