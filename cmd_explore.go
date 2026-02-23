1adb26782f2dd6c5a92cd438ecf9c25bd6fda87d
Denkkou <JoeSchoDev@gmail.com>
2026-02-23 12:20:27 +0200

    Implemented explore command to list which Pokemon encounters are in given location.

--- cmd_explore.go
+++ cmd_explore.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
 
 func commandExplore(config *config, areaName string) error {
-	fmt.Printf("Exploring: %s\n", areaName)
+	if areaName == "" {
+		return errors.New("Area name invalid.")
+	}
+
+	// Get explore data
+	exploreResponse, err := config.client.Explore(areaName)
+	if err != nil {
+		return err
+	}
+
+	// Print results
+	fmt.Printf("Exploring %s...\n", exploreResponse.Name)
+	fmt.Println("Found Pokemon: ")
+	for _, encounter := range exploreResponse.PokemonEncounters {
+		fmt.Printf(" - %s\n", encounter.Pokemon.Name)
+	}
 	return nil
 }
--- internal/pokeapi/client_explore.go
+++ internal/pokeapi/client_explore.go
@@ -1,14 +1,52 @@
 package pokeapi
 
 import (
-
+	"net/http"
+	"encoding/json"
+	"io"
 )
 
 // Retrieve from HTTP or cache ExploreData pertaining to given areaName
 func (c *Client) Explore(areaName string) (ExploreData, error) {
-	//fullURL := baseURL + "/location-area" + areaName
+	fullURL := baseURL + "/location-area/" + areaName
 
+	// Check cache for an entry
+	entry, ok := c.cache.Get(fullURL)
+	if ok {
+		exploreDataResponse := ExploreData{}
+		err := json.Unmarshal(entry, &exploreDataResponse)
+		if err != nil {
+			return ExploreData{}, err
+		}
+		return exploreDataResponse, nil
+	}
 
+	// Build request
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return ExploreData{}, err
+	}
 
-	return ExploreData{}, nil
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return ExploreData{}, nil
+	}
+	defer res.Body.Close()
+
+	// Unmarshal
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return ExploreData{}, nil
+	}
+
+	// Cache data
+	c.cache.Add(fullURL, data)
+
+	exploreDataResponse := ExploreData{}
+	err = json.Unmarshal(data, &exploreDataResponse)
+	if err != nil {
+		return ExploreData{}, err
+	}
+
+	return exploreDataResponse, nil
 }
